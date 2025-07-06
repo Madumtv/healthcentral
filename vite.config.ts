278525697fdd1d11bc52958f1e47dad178c976c2
Madum <marin.dumont@gmail.com>
2025-07-06 13:56:03 +0200

    Add basic Vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  test: {
+    environment: 'jsdom',
+  },
   server: {
     host: "::",
     port: 8080,
